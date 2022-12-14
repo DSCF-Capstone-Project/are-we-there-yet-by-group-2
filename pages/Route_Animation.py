94d4b03a39ea94a04abe9d852305d5e74b967e5a
Bianca Isabelle Gilay <isagilay.aws@gmail.com>
2022-12-15 00:45:24 +0900

    changed the gif file to one with a lower size

--- pages/Route_Animation.py
+++ pages/Route_Animation.py
@@ -3,12 +3,12 @@ import pybase64 as base64
 
 st.set_page_config(page_title="Sa'n Ka Punta?")
 
-# file_ = open("./media/animation.gif", "rb")
-# contents = file_.read()
-# data_url = base64.b64encode(contents).decode("utf-8")
-# file_.close()
+file_ = open("./media/compressed.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
 
-# st.markdown(
-#     f'<img src="data:image/gif;base64,{data_url}" alt="routes gif">',
-#     unsafe_allow_html=True,
-# )
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="routes gif">',
+    unsafe_allow_html=True,
+)
